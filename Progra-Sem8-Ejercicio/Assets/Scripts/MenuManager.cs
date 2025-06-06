0312243746f529b9735a63b467c4ca906eb490d7
Miguelitox36 <miguelitox36@hotmail.com>
2025-06-05 22:25:36 -0500

    UpdateButtoms

--- Progra-Sem8-Ejercicio/Assets/Scripts/MenuManager.cs
+++ Progra-Sem8-Ejercicio/Assets/Scripts/MenuManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using GameJolt.API;
+using GameJolt.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -15,7 +17,7 @@ public class MenuManager : MonoBehaviour
         {
             trophiesButton.onClick.AddListener(() =>
             {                
-                Debug.Log("Mostrar UI de Trofeos internos (si existe).");
+                GameJoltUI.Instance.ShowTrophies();
             });
         }
         else Debug.LogError("Trophies Button no asignado en MenuManager.");
@@ -32,8 +34,8 @@ public class MenuManager : MonoBehaviour
         if (rankingButton != null)
         {
             rankingButton.onClick.AddListener(() =>
-            {                
-                Debug.Log("Mostrar UI de Ranking internos (si existe).");
+            {
+                GameJoltUI.Instance.ShowLeaderboards();
             });
         }
         else Debug.LogError("Ranking Button no asignado en MenuManager.");
